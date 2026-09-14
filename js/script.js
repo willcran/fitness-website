10664893d54b3c4eb967a5c0ff91b9570c82515e
Will Cranston <188920037+devster3000@users.noreply.github.com>
2026-09-14 14:40:55 +0000

    Added colour themes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-const html = document.documentElement;
-const button = document.getElementsByClassName("theme-button");
+console.log("script.js loaded")
 
+const html = document.documentElement;
+const themeButton = document.getElementById("themeButton");
+const themeIcon = document.getElementById("themeIcon");
+
+/* Loads the preferred theme from storage */
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme !== "system") {
+    html.setAttribute("theme", savedTheme);
+}
+
+/* Cycles through the 3 options */
+function themeCycle() {
+    const currentTheme = localStorage.getItem("theme");
+
+    /* Changes system theme to light mode */
+
+    if (!currentTheme || currentTheme === "system"){
+        html.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+
+        themeIcon.src = "assets/icons/sun_light.png"
+    }
+
+    /* If on light mode, next button click is dark */
+    else if (currentTheme === "light") {
+        html.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+
+        themeIcon.src = "assets/icons/moon_light.png"
+    }
+
+    /* Change dark to system theme */
+    else if (currentTheme === "dark") {
+        html.removeAttribute("data-theme");
+        localStorage.setItem("theme", "system");
+
+        themeIcon.src = "assets/icons/computer_light.png"
+    }
+}
+
+themeButton.addEventListener("click", themeCycle);
