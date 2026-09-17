2499af34cf49adc86f76a976bca100e5798a2754
Will Cranston <188920037+devster3000@users.noreply.github.com>
2026-09-17 12:51:52 +0000

    Added saved themes across the site.

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ const themeIcon = document.getElementById("themeIcon");
 
 /* Loads the preferred theme from storage */
 const savedTheme = localStorage.getItem("theme");
+const savedIcon = localStorage.getItem("icon");
 
 if (savedTheme !== "system") {
-    document.body.classList.add(savedTheme)
-    html.setAttribute("theme", savedTheme);
+    html.setAttribute("data-theme", savedTheme);
 }
+
+themeIcon.src = savedIcon;
 
 /* Cycles through the 3 options */
 function themeCycle() {
@@ -21,24 +23,27 @@ function themeCycle() {
     if (!currentTheme || currentTheme === "system"){
         html.setAttribute("data-theme", "light");
         localStorage.setItem("theme", "light");
+        localStorage.setItem("icon", "/assets/icons/sun_light.png");
 
-        themeIcon.src = "assets/icons/sun_light.png"
+        themeIcon.src = localStorage.getItem("icon");
     }
 
     /* If on light mode, next button click is dark */
     else if (currentTheme === "light") {
         html.setAttribute("data-theme", "dark");
         localStorage.setItem("theme", "dark");
+        localStorage.setItem("icon", "/assets/icons/moon_light.png");
 
-        themeIcon.src = "assets/icons/moon_light.png"
+        themeIcon.src = localStorage.getItem("icon");
     }
 
     /* Change dark to system theme */
     else if (currentTheme === "dark") {
         html.removeAttribute("data-theme");
         localStorage.setItem("theme", "system");
+        localStorage.setItem("icon", "/assets/icons/computer_light.png");
 
-        themeIcon.src = "assets/icons/computer_light.png"
+        themeIcon.src = localStorage.getItem("icon");
     }
 }
 
